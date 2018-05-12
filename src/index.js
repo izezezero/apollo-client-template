d8164035b23fb92cab646334388a00fa929b88fc
izezero <icepanda501@gmail.com>
2018-05-12 14:20:51 +0700

    test1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import client from './graphql';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+    <ApolloProvider client={client} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
 registerServiceWorker();
