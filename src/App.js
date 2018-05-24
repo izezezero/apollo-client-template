d5267fa716c8e9dd576808f9fffed44297ec80e4
eaotnc <en.tnc@hotmail.com>
2018-05-24 18:34:48 +0700

    insert customer

--- src/App.js
+++ src/App.js
@@ -3,34 +3,31 @@ import gql from 'graphql-tag'
 import React, { Component } from 'react'
 
 import './App.css'
-import logo from './logo.svg'
 
 class App extends Component {
   state = {
-    createUserInput: {
+    createCustomerInput: {
+      id: 0,
       firstName: '',
       lastName: '',
+      fullName: '',
+      email: '',
+      website: '',
     },
   }
-  handleFirstName(e) {
+
+  handleChange = (e) => {
     this.setState({
-      createUserInput: {
-        ...this.state.createUserInput,
-        firstName: e.target.value,
+      createCustomerInput: {
+        ...this.state.createCustomerInput,
+        [e.target.name]: e.target.value,
       },
     })
   }
-  handleLastName(e) {
-    this.setState({
-      createUserInput: {
-        ...this.state.createUserInput,
-        lastName: e.target.value,
-      },
-    })
-  }
-  async createUser() {
-    console.log(this.state.createUserInput)
-    await this.props.createUser({ variables: { createUserInput: this.state.createUserInput } })
+
+  async createCustomer() {
+    console.log(this.state.createCustomerInput)
+    await this.props.createCustomer({ variables: { createCustomerInput: this.state.createCustomerInput } })
     await this.props.refetch()
   }
   render() {
@@ -41,13 +38,81 @@ class App extends Component {
         </header>
         <div className="App-body">
           <div className="input-group">
-            <span>firstName</span><input className="text-input" onChange={e => this.handleFirstName(e)} value={this.state.createUserInput.firstName} />
-            <span>lastName</span><input className="text-input" onChange={e => this.handleLastName(e)} value={this.state.createUserInput.lastName} />
-            <button className="button-input" onClick={() => this.createUser()}>CREATE</button>
+
+            <span>id</span>
+            <input
+              className="text-input"
+              value={this.state.createCustomerInput.id}
+              name="id"
+              onChange={e => this.handleChange(e)}
+            />
+
+            <span>firstName</span>
+            <input
+              className="text-input"
+              name="firstName"
+              onChange={e => this.handleChange(e)}
+              value={this.state.createCustomerInput.firstName}
+            />
+
+            <span>lastName</span>
+            <input
+              className="text-input"
+              name="lastName"
+              onChange={e => this.handleChange(e)}
+              value={this.state.createCustomerInput.lastName}
+            />
+
+            <span>fullName</span>
+            <input
+              className="text-input"
+              name="fullName"
+              onChange={e => this.handleChange(e)}
+              value={this.state.createCustomerInput.fullName}
+            />
+
+            <span>email</span>
+            <input
+              className="text-input"
+              name="email"
+              onChange={e => this.handleChange(e)}
+              value={this.state.createCustomerInput.email}
+            />
+
+            <span>website</span>
+            <input
+              className="text-input"
+              name="website"
+              onChange={e => this.handleChange(e)}
+              value={this.state.createCustomerInput.website}
+            />
+
+            <button className="button-input" onClick={() => this.createCustomer()}>CREATE</button>
           </div>
           <div className="nameCardGroup">
             {
-              this.props.data.users.map(user => (<div className="nameCard">{user.fullName}</div>))
+              this.props.data.customers.map(customer => (
+                <div key={customer.id} className="nameCard">
+                  {customer.fullName}<br />
+                  <div className="smallBlock">
+                    CustomerData <br />
+                    firstName: {customer.firstName}<br />
+                    LastName: {customer.lastName} <br />
+                    Email: {customer.email} <br />
+                    Website: {customer.website} <br />
+                  </div>
+                  <div className="smallBlock">
+                    {
+                      customer.company.map(company => (
+                        <div key={company.id}>
+                          CompanyData<br />
+                          id: {company.id}<br />
+                          name: {company.name} <br />
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              ))
             }
           </div>
         </div>
@@ -56,38 +121,57 @@ class App extends Component {
   }
 }
 
-const getCurrentUser = gql`
-  query GetCurrentUser {
-            users {
+const getCurrentCustomer = gql`
+  query GetCurrentCustomer {
+            customers{
+          id
+          firstName
+          lastName
           fullName
+          email
+          website
+          company{
+            id
+            name
+          }
         }
       }
     `
 
-const createUser = gql`
-  mutation createUser($createUserInput : CreateUserInput){
-    createUser(createUserInput :$createUserInput){
-      fullName
-    }
-  }
-`
+const createCustomer = gql`
+  mutation createCustomer($createCustomerInput : CreateCustomerInput){
+            createCustomer(createCustomerInput : $createCustomerInput){
+              id
+              firstName
+              lastName
+              fullName
+              email
+              website
+                company{
+                  id
+                  name
+                }
+              }
+          }
+        `
 const AppBindQuery = () => (
   <Mutation
-    mutation={createUser}
-    update={(cache, { data: { createUser } }) => {
-      const { users } = cache.readQuery({ query: getCurrentUser })
+    mutation={createCustomer}
+    update={(cache, { data: { createCustomer } }) => {
+      const { customers } = cache.readQuery({ query: getCurrentCustomer })
+      console.log('creactCustomer',)
       cache.writeQuery({
-        query: getCurrentUser,
-        data: { users: users.concat(createUser) },
+        query: getCurrentCustomer,
+        data: { customers: customers.concat(createCustomer) },
       })
     }}
   >
     {
-      (createUser, data) => (
-        <Query query={getCurrentUser}>
+      (createCustomer, data) => (
+        <Query query={getCurrentCustomer}>
           {
             ({ loading, data, refetch }) => (!loading ? (
-              <App data={data} createUser={createUser} refetch={refetch} />
+              <App data={data} createCustomer={createCustomer} refetch={refetch} />
             ) : null)
           }
         </Query>
